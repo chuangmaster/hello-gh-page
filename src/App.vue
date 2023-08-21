2a2f194cb41760d5413a75050912af3b1ec72dae
chuangmaster <chuang.master@gmail.com>
2023-08-21 10:39:00 +0800

    test view

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/pomelo2022">About22</router-link>
+
   </nav>
   <router-view/>
 </template>
